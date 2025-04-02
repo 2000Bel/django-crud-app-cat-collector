bc5709a9980e0f6ee60fe901a684e787f66cb87c
2000Bel <belvelizm@gmail.com>
2025-04-02 19:26:58 -0400

    Cat done

---
+++ main_app/urls.py
@@ -0,0 +1,12 @@
+
+#define the route
+from django.urls import path
+from . import views  # Import views.py
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('cats/', views.cat_index, name='cat-index'),
+    path('cats/<int:cat_id>/', views.cat_detail, name='cat-detail'),
+]
+
