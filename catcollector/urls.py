664cc62800c0aef6141211daa5bffbcff0e0446e
2000Bel <belvelizm@gmail.com>
2025-04-03 18:38:40 -0400

    Cat done

--- catcollector/urls.py
+++ catcollector/urls.py
@@ -2,10 +2,10 @@
 URL configuration for catcollector project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
 Examples:
 Function views
-    1. Add an import:  from main_app import views
+    1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
@@ -14,15 +14,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# catcollector/urls.py
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('main_app.urls')), # Mounts main_app's routes at the root URL
+    path('', include('main_app.urls')),
 ]
-
-
-
-
--- main_app/views.py
+++ main_app/views.py
@@ -1,38 +1,79 @@
-from django.shortcuts import render
-from .models import Cat
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
+from .forms import FeedingForm
+
 from django.http import HttpResponse
 
-class Cat:
-    def __init__(self, name, breed, description, age):
-        self.name = name
-        self.breed = breed
-        self.description = description
-        self.age = age
-
-# Create a list of Cat instances
-cats = [
-    Cat('Lolo', 'tabby', 'Kinda rude.', 3),
-    Cat('Sachi', 'tortoiseshell', 'Looks like a turtle.', 0),
-    Cat('Fancy', 'bombay', 'Happy fluff ball.', 4),
-    Cat('Bonk', 'selkirk rex', 'Meows loudly.', 6)
-]
-
-# create the views here
+# Create your views here.
 
 def home(request):
-    return render(request, 'home.html')
+  return render(request, 'home.html')
 
 def about(request):
-    return render(request, 'about.html')
+  return render(request, 'about.html')
 
 def cat_index(request):
-    cats = Cat.objects.all()
-    return render(request, 'cats/index.html', {'cats': cats})
+  cats = Cat.objects.all()
+  return render(request, 'cats/index.html', { 'cats': cats })
 
 def cat_detail(request, cat_id):
-    cat = Cat.objects.get(id=cat_id)
-    return render(request, 'cats/detail.html', {'cat': cat})
+  cat = Cat.objects.get(id=cat_id)
 
+  toys_cat_doesnt_have = Toy.objects.exclude(id__in = cat.toys.all().values_list('id'))
 
+  feeding_form = FeedingForm()
+  return render(request, 'cats/detail.html', { 
+    'cat': cat, 'feeding_form': feeding_form, 'toys': toys_cat_doesnt_have
+  })
 
+class CatCreate(CreateView):
+  model = Cat
+  fields = ['name', 'breed', 'description', 'age']
 
+class CatUpdate(UpdateView):
+  model = Cat
+  fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+  model = Cat
+  success_url = '/cats/'
+
+def add_feeding(request, cat_id):
+  form = FeedingForm(request.POST)
+
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  
+  return redirect('cat-detail', cat_id=cat_id)
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+
+def associate_toy(request, cat_id, toy_id):
+  Cat.objects.get(id=cat_id).toys.add(toy_id)
+  return redirect('cat-detail', cat_id=cat_id)
+
+def remove_toy(request, cat_id, toy_id):
+  cat = Cat.objects.get(id=cat_id)
+  toy = Toy.objects.get(id=toy_id)
+  cat.toys.remove(toy_id)
+  return redirect('cat-detail', cat_id=cat.id)
